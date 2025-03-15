78266cc7ad1f190370ec9dc8b810f03f44e7fd54
necrology <necrology69@gmail.com>
2025-03-16 04:34:49 +0700

    notice ketika data kosong

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -163,78 +163,89 @@ function truncateDescription(description, maxLength = 500) {
                 </header>
 
                 <main class="mt-6">
-                    <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
-                        <a
-                            v-for="(book, index) in paginatedBooks"
-                            :key="books.id"
-                            :href="route('books.show', book.id)"
-                            id="docs-card"
-                            class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
-                        >
-                            <div
-                                id="screenshot-container"
-                                class="relative flex w-full flex-1 items-stretch"
-                            >
-                                <img
-                                    :src="book.thumbnail"
-                                    alt="Laravel documentation screenshot"
-                                    class="aspect-video h-full w-full flex-1 rounded-[10px] object-cover object-top drop-shadow-[0px_4px_34px_rgba(0,0,0,0.06)] dark:hidden"
-                                    @error="handleImageError"
-                                />
-                                <div
-                                    class="absolute -bottom-16 -left-16 h-40 w-[calc(100%+8rem)] bg-gradient-to-b from-transparent via-white to-white dark:via-zinc-900 dark:to-zinc-900"
-                                ></div>
-                            </div>
-
-                            <div
-                                class="relative flex items-center gap-6 lg:items-end"
+                    <template v-if="paginatedBooks.length > 0">
+                        <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
+                            <a
+                                v-for="(book, index) in paginatedBooks"
+                                :key="books.id"
+                                :href="route('books.show', book.id)"
+                                id="docs-card"
+                                class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                             >
                                 <div
-                                    id="docs-card-content"
-                                    class="flex items-start gap-6 lg:flex-col"
+                                    id="screenshot-container"
+                                    class="relative flex w-full flex-1 items-stretch"
                                 >
-                                    <div class="pt-3 sm:pt-5 lg:pt-0">
-                                        <h2
-                                            class="text-xl font-semibold text-black dark:text-white"
-                                        >
-                                            {{ book.title }}
-                                        </h2>
-
-                                        <h2
-                                            class="mt-1 font-semibold text-black dark:text-white"
-                                        >
-                                            Author : {{ book.author }}
-                                        </h2>
-
-                                        <p class="mt-4 text-sm/relaxed">
-                                            {{
-                                                truncateDescription(
-                                                    book.description
-                                                )
-                                            }}
-                                        </p>
-                                        <p class="mt-4 text-sm/relaxed">
-                                            Rating : ⭐ {{ book.rating }}/5
-                                        </p>
-                                    </div>
+                                    <img
+                                        :src="book.thumbnail"
+                                        alt="Laravel documentation screenshot"
+                                        class="aspect-video h-full w-full flex-1 rounded-[10px] object-cover object-top drop-shadow-[0px_4px_34px_rgba(0,0,0,0.06)] dark:hidden"
+                                        @error="handleImageError"
+                                    />
+                                    <div
+                                        class="absolute -bottom-16 -left-16 h-40 w-[calc(100%+8rem)] bg-gradient-to-b from-transparent via-white to-white dark:via-zinc-900 dark:to-zinc-900"
+                                    ></div>
                                 </div>
 
-                                <svg
-                                    class="size-6 shrink-0 stroke-[#FF2D20]"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
+                                <div
+                                    class="relative flex items-center gap-6 lg:items-end"
                                 >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-                                    />
-                                </svg>
-                            </div>
-                        </a>
-                    </div>
+                                    <div
+                                        id="docs-card-content"
+                                        class="flex items-start gap-6 lg:flex-col"
+                                    >
+                                        <div class="pt-3 sm:pt-5 lg:pt-0">
+                                            <h2
+                                                class="text-xl font-semibold text-black dark:text-white"
+                                            >
+                                                {{ book.title }}
+                                            </h2>
+
+                                            <h2
+                                                class="mt-1 font-semibold text-black dark:text-white"
+                                            >
+                                                Author : {{ book.author }}
+                                            </h2>
+
+                                            <p class="mt-4 text-sm/relaxed">
+                                                {{
+                                                    truncateDescription(
+                                                        book.description
+                                                    )
+                                                }}
+                                            </p>
+                                            <p class="mt-4 text-sm/relaxed">
+                                                Rating : ⭐ {{ book.rating }}/5
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                    <svg
+                                        class="size-6 shrink-0 stroke-[#FF2D20]"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+                                        />
+                                    </svg>
+                                </div>
+                            </a>
+                        </div>
+                    </template>
+                    <span v-else>
+                        <div class="flex justify-center mt-6 space-x-2">
+                            <h2
+                                class="text-xl font-semibold text-black dark:text-white"
+                            >
+                                Tidak ada data.
+                            </h2>
+                        </div>
+                    </span>
                     <div class="flex justify-center mt-6 space-x-2">
                         <button
                             @click="goToPage(currentPage - 1)"
